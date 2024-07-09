a260b316b511ad98ed7a07a31e133a8d17a281c8
webdeveloper510 <developerweb6@gmail.com>
2024-07-09 15:54:57 +0530

    products payment status

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -11,6 +11,7 @@ use App\Models\DeliveryCost;
 use App\Models\DriverHistory;
 use App\Models\SubProduct;
 use Carbon\Carbon;
+use App\Models\Product;
 
 class OrderRepository extends Repository
 {
@@ -63,6 +64,13 @@ class OrderRepository extends Repository
             'order_status' => config('enums.payment_status.pending'),
             'address_id' => $address_id,
             'instruction' => $request->instruction
+        ]);
+
+
+        $products = new Product();
+        $products->update([
+            'subscription_status' => 1,
+            'subscription_type' => Product::find($productid)->name,
         ]);
 
         //$order = [$productid, ['quantity' => 1],['orderID' => $order->id]];
