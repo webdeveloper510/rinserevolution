0934f82ed2900a54d16844057718a3a9c0acb5f4
webdeveloper510 <developerweb6@gmail.com>
2024-08-14 16:31:33 +0530

    feat: privacy policy

--- resources/views/settings/privacy-policy.blade.php
+++ resources/views/settings/privacy-policy.blade.php
@@ -1,86 +1,82 @@
 <html>
-<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
-<style type="text/css">
-    <!--
-    span.cls_003{font-family:"Calibri",serif;font-size:14.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
-    div.cls_003{width: 80%;font-family:"Calibri",serif;font-size:14.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
 
-    .mt{
-        margin-top: 1em;
-    }
-    h4{
-        margin: 0;
-    }
-    -->
-</style>
+<head>
+    <meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+    <style type="text/css">
+        <!--
+        span.cls_003 {
+            font-family: "Calibri", serif;
+            font-size: 14.1px;
+            color: rgb(0, 0, 0);
+            font-weight: normal;
+            font-style: normal;
+            text-decoration: none
+        }
+
+        div.cls_003 {
+            width: 80%;
+            font-family: "Calibri", serif;
+            font-size: 14.1px;
+            color: rgb(0, 0, 0);
+            font-weight: normal;
+            font-style: normal;
+            text-decoration: none
+        }
+
+        .mt {
+            margin-top: 1em;
+        }
+
+        h4 {
+            margin: 0;
+        }
+        -->
+    </style>
 </head>
+
 <body>
-    <div style="position:absolute;left:50%;margin-left:-306px;top:0px;width:612px;height:792px;border-style:outset;overflow:hidden">
-        <div style="position:absolute;left:72.02px;top:100.34px" class="cls_003">
-            <h4>Privacy policy</h4>
-            <span class="cls_003">
-                Goldstardrycleaners has developed Goldstardrycleaners app as a free app.  If anyone decides to use our services, this page is used to inform visitors about our policies, including the collection, use and disclosure of personal information. If you wish to use our Services, you agree to the collection and use of information related to this policy, the personal information we collect is used for service delivery and improvement. We will not use or share your information with anyone except as described in this Privacy Policy. The terms used in this Privacy Policy have the same meaning as our Terms and Conditions, which are accessible on goldstardrycleaners.com unless otherwise defined in this Privacy Policy.
-            </span>
-
-            <h4 class="mt">Data collection and use</h4>
-            <span class="cls_003">
-                For a better experience, when using our services, we want to provide you with some personally identifiable information, including but not limited to email and phone numbers. The information we request will be retained and used as described in this Privacy Policy. The app uses third party services that may collect information used to identify you. Link to the privacy policy of third party service providers used by the app
-                # <a href="#">Google Play Services</a>
-            </span>
-
-            <h4 class="mt">Log data</h4>
-            <span class="cls_003">
-                We would like to inform you that whenever you use our services, in case of any error in the app, we collect data and information (through third party products) on your phone called log data. This log data may include information such as your device's Internet Protocol ("IP") address, device name, operating system version,
-                app configuration when using our Services, the time and date of your use of the Service, and other statistics.
-            </span>
-
-            <h4 class="mt">Cookies</h4>
-            <span class="cls_003">
-                Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. Websites you visit are sent to your browser and stored in your device's internal memory. This service does not explicitly use these "cookies". However, the app may use third-party code and libraries that use "cookies" to collect information and improve their services. You have the option of accepting or rejecting these cookies and knowing when a cookie is being sent to your device. If you wish to reject our cookies, you will not be able to use any part of this service.
-            </span>
-
-        </div>
-    </div>
-
-    <div style="position:absolute;left:50%;margin-left:-306px;top:802px;width:612px;height:792px;border-style:outset;overflow:hidden">
-        <div style="position:absolute;left:72.02px;top:89.78px" class="cls_003">
-            <h4>Service provider</h4>
-            <span class="cls_003">We may hire third party companies and individuals for the following reasons: <br>
-                # To facilitate our services. <br>
-                # Provide services on our behalf. <br>
-                # Perform service-related services Or <br>
-                # To help us analyze how our services are used. <br>
-                We want to let users of this service know that this third party has access to your personal information. The reason is to perform the tasks assigned to them by us. However, they are obliged not to disclose or use the information for any other purpose.
-
-            </span>
-
-            <h4 class="mt">Security</h4>
-            <span class="cls_003">
-                We value your trust in providing your personal information, so we're trying to use commercially acceptable means to protect it. But keep in mind that any method of transmission, or electronic storage, via the Internet is not 100% secure and reliable and we cannot guarantee its complete security.
-            </span>
-
-            <h4 class="mt">Links to other sites</h4>
-            <span class="cls_003">
-                This service may contain links to other sites. If you click on a third party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly recommend that you review the privacy policies of these websites. We have no control over or liability for the content, privacy policies, or
-                practices of any third party sites or services.
-            </span>
-
-            <h4 class="mt">Children's privacy</h4>
-            <span class="cls_003">
-                These services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under the age of 13. In the event that we discover that a child under the age of 13 has provided us with personal information, we immediately delete it from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we can take the necessary action.
-            </span>
-
-            <h4 class="mt">This changes the privacy policy</h4>
-            <span class="cls_003">
-                We may update our Privacy Policy from time to time. So, you are advised to periodically review this page for any changes. We will notify you of any changes by posting a new Privacy Policy on this page.
-            </span>
-
-            <h4 class="mt">Contact</h4>
-            <span class="cls_003">
-                If you have any questions or suggestions regarding our Privacy Policy, feel free to contact us at <a href="#">info@goldstardrycleaning.co.uk</a>
-            </span>
-        </div>
-    </div>
+    <h3><strong>Effective Date: June 1, 2024</strong></h3>
+    <h4><strong>1. Introduction</strong></h4>
+    <p>Welcome to the Rinse Revolution website ("we", "our", "us"). We are committed to protecting and respecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit our website [<a href="http://www.rinserevolution.com/">www.rinserevolution.com</a>] or use our services. Please read this policy carefully to understand our views and practices regarding your personal data and how we will treat it.</p>
+    <h4><strong>2. Information We Collect</strong></h4>
+    <p>We may collect and process the following data about you:</p>
+    <ul>
+        <li><strong>Personal Identification Information:</strong>&nbsp;Name, address, email address, phone number.</li>
+        <li><strong>Payment Information:</strong>&nbsp;Credit/debit card details, billing address.</li>
+        <li><strong>Service Information:</strong>&nbsp;Details about your laundry orders, including pickup and delivery times, types of garments, and special instructions.</li>
+        <li><strong>Technical Data:</strong>&nbsp;IP address, browser type, operating system, access times, and referring website addresses.</li>
+        <li><strong>Location Data:</strong>&nbsp;Real-time location information necessary to provide pickup and delivery services.</li>
+        <li><strong>Cookies and Tracking Technologies:</strong>&nbsp;We use cookies and similar tracking technologies to track the activity on our website and hold certain information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent.</li>
+    </ul>
+    <h4><strong>3. How We Use Your Information</strong></h4>
+    <p>We use the information we collect in the following ways:</p>
+    <ul>
+        <li>To provide, operate, and maintain our services.</li>
+        <li>To process transactions and send related information, including transaction confirmations and receipts.</li>
+        <li>To manage your account and provide customer support.</li>
+        <li>To communicate with you, including sending service-related messages, updates, and promotional materials.</li>
+        <li>To personalize your experience and analyze trends to improve our services.</li>
+        <li>To detect, prevent, and address technical issues and ensure the security of our services.</li>
+        <li>To comply with legal obligations and protect our rights and the rights of others.</li>
+    </ul>
+    <h4><strong>4. How We Share Your Information</strong></h4>
+    <p>We do not sell, trade, or otherwise transfer your personal information to outside parties except as described below:</p>
+    <ul>
+        <li><strong>Service Providers:</strong>&nbsp;We may share your information with third-party vendors, service providers, and agents who perform services on our behalf, such as payment processing, order fulfillment, and customer support.</li>
+        <li><strong>Legal Requirements:</strong>&nbsp;We may disclose your information if required to do so by law or in response to valid requests by public authorities.</li>
+        <li><strong>Business Transfers:</strong>&nbsp;In the event of a merger, acquisition, or sale of all or a portion of our assets, your information may be transferred to the new owner.</li>
+    </ul>
+    <h4><strong>5. Data Security</strong></h4>
+    <p>We use administrative, technical, and physical security measures to help protect your personal information. While we have taken reasonable steps to secure the personal information you provide, please be aware that no security measures are perfect or impenetrable, and no method of data transmission can be guaranteed against any interception or other type of misuse.</p>
+    <h4><strong>Policy Declaration - Data Safety Section</strong>: Device Or Other IDs Data Type - Device Or Other IDs (some common examples may include Advertising ID, Android ID, IMEI, BSSID, MAC address)</h4>
+    <p>&nbsp;</p>
+    <h4><strong>How to reach out:&nbsp;</strong></h4>
+    <h4><br />Ho if you have any issue in using the app or want to provide valuable suggestions you could reach out to us by this email and address.</h4>
+    <h4><br /><strong>info@lotusus.com</strong></h4>
+    <h3>lotus Analytics</h3>
+    <h4>101 River Street, Warrensburg, NY, 12885, US</h4>
+    <p><a href="https://lotusanalytics.com/"><strong>https://lotusanalytics.com/</strong></a></p>
 
 </body>
+
 </html>
