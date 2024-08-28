7711a8b41108c90b7b217057623de28f9ebf6321
webdeveloper510 <developerweb6@gmail.com>
2024-08-28 15:49:21 +0530

    api: payment testing

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -23,11 +23,14 @@ class ProductResource extends JsonResource
             }
         }
 
-
-        foreach ($this->payments as $key_pay => $value_pay) {
-            if (!empty($value_pay->order->products[0])) {
-                $match_case = $value_pay->users_id == $this->login_user && $this->id == $value_pay->order->products[0]->id  ? true : false;
+        if (isset($this->payments) && $this->payments->isNotEmpty()) {
+            foreach ($this->payments as $key_pay => $value_pay) {
+                if (!empty($value_pay->order->products[0])) {
+                    $match_case = $value_pay->users_id == $this->login_user && $this->id == $value_pay->order->products[0]->id  ? true : false;
+                }
             }
+        } else {
+            $match_case = '';
         }
 
         return [
